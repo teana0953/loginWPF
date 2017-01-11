8ec2079db54387ef118dd78c974932c6269c94c0
tina.liang <tina.liang@microlife.com.tw>
2017-01-11 15:36:10 +0800

    Facebook log in OK

--- LoginWpf/LoginDialog.xaml.cs
+++ LoginWpf/LoginDialog.xaml.cs
@@ -11,17 +11,88 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Facebook;
+using System.Dynamic;
 
 namespace LoginWpf
 {
+    public enum LogIn
+    {
+        fb,
+        google
+    }
     /// <summary>
     /// LoginDialog.xaml 的互動邏輯
     /// </summary>
     public partial class LoginDialog : Window
     {
-        public LoginDialog(string url)
+        private const string appId = "1835738026681907";    // from https://developers.facebook.com/apps/
+        /// <summary>
+        /// Extended permissions is a comma separated list of permissions to ask the user.
+        /// </summary>
+        /// <remarks>
+        /// For extensive list of available extended permissions refer to 
+        /// https://developers.facebook.com/docs/reference/api/permissions/
+        /// </remarks>
+        private const string extendedPermissions = "public_profile,user_birthday";
+        private readonly Uri loginUrl;
+        public FacebookOAuthResult FacebookOAuthResult { get; private set; }
+        protected FacebookClient fb = new FacebookClient();
+        public LoginDialog(LogIn way)
         {
+            switch (way)
+            {
+                case LogIn.fb:
+                    loginUrl = GenerateLoginUrl(appId,extendedPermissions);
+                    break;
+                case LogIn.google:
+                    break;
+            }
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            loginBrowser.Navigate(loginUrl.AbsoluteUri);
+        }
+
+        private Uri GenerateLoginUrl(string appId, string extendedPermissions)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.client_id = appId;
+            parameters.redirect_uri = "https://www.facebook.com/connect/login_success.html";
+
+            // The requested response: an access token (token), an authorization code (code), or both (code token).
+            parameters.response_type = "token";
+
+            // list of additional display modes can be found at http://developers.facebook.com/docs/reference/dialogs/#display
+            parameters.display = "popup";
+
+            // add the 'scope' parameter only if we have extendedPermissions.
+            if (!string.IsNullOrWhiteSpace(extendedPermissions))
+                parameters.scope = extendedPermissions;
+
+            // when the Form is loaded navigate to the login url.
+            return fb.GetLoginUrl(parameters);
+        }
+
+        private void loginBrowser_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            // whenever the browser navigates to a new url, try parsing the url.
+            // the url may be the result of OAuth 2.0 authentication.
+
+            FacebookOAuthResult oauthResult;
+            if (fb.TryParseOAuthCallbackUrl(e.Uri, out oauthResult))
+            {
+                // The url is the result of OAuth 2.0 authentication
+                FacebookOAuthResult = oauthResult;
+                DialogResult = FacebookOAuthResult.IsSuccess;
+            }
+            else
+            {
+                // The url is NOT the result of OAuth 2.0 authentication.
+                FacebookOAuthResult = null;
+            }
         }
     }
 }
--- LoginWpf/MainWindow.xaml.cs
+++ LoginWpf/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Facebook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +22,7 @@ namespace LoginWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,12 +30,114 @@ namespace LoginWpf
 
         private void btn_fbLogin_Click(object sender, RoutedEventArgs e)
         {
-
+            LoginDialog logInDialog = new LoginDialog(LogIn.fb);
+            logInDialog.ShowDialog();
+            // Take log in action
+            if (logInDialog.FacebookOAuthResult == null)
+            {
+                // the user closed the FacebookLoginDialog, so do nothing.
+                MessageBox.Show("Cancelled!");
+                return;
+            }
+            // Even though facebookOAuthResult is not null, it could had been an 
+            // OAuth 2.0 error, so make sure to check IsSuccess property always.
+            if (logInDialog.FacebookOAuthResult.IsSuccess)
+            {
+                // since our respone_type in FacebookLoginDialog was token,
+                // we got the access_token
+                // The user now has successfully granted permission to our app.
+                GetProfileFromFb(logInDialog.FacebookOAuthResult.AccessToken);
+            }
+            else
+            {
+                // for some reason we failed to get the access token.
+                // most likely the user clicked don't allow.
+                MessageBox.Show(logInDialog.FacebookOAuthResult.ErrorDescription);
+            }
         }
 
         private void btn_goolgeLogin_Click(object sender, RoutedEventArgs e)
         {
+            LoginDialog logInDialog = new LoginDialog(LogIn.google);
+            logInDialog.ShowDialog();
+        }
 
+        private void GetProfileFromFb(string accessKey)
+        {
+            try
+            {
+                var fb = new FacebookClient(accessKey);
+
+                // FacebookClient's Get/Post/Delete methods only supports JSON response results.
+                // For non json results, you will need to use different mechanism,
+
+                // load other profile info
+                fb.GetCompleted += (o, e) =>
+                {
+                    // incase you support cancellation, make sure to check
+                    // e.Cancelled property first even before checking (e.Error != null).
+                    if (e.Cancelled)
+                    {
+                        // for this example, we can ignore as we don't allow this
+                        // example to be cancelled.
+                    }
+                    else if (e.Error != null)
+                    {
+                        // error occurred
+                        Application.Current.Dispatcher.Invoke(()=>
+                            { MessageBox.Show(e.Error.Message); }
+                        );
+                    }
+                    else
+                    {
+                        // the request was completed successfully
+
+                        // now we can either cast it to IDictionary<string, object> or IList<object>
+                        // depending on the type.
+                        // For this example, we know that it is IDictionary<string,object>.
+                        var result = (IDictionary<string, object>)e.GetResultData();
+
+                        var id = (string)result["id"];
+                        var name = (string)result["name"];
+                        var firstName = (string)result["first_name"];
+                        var lastName = (string)result["last_name"];
+                        var gender = (string)result["gender"];
+                        var dob = (string)result["birthday"];
+
+                        // since this is an async callback, make sure to be on the right thread
+                        // when working with the UI.
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            tb_Id.Text = id;
+                            tb_name.Text = name;
+                            tb_gender.Text = gender;
+                            tb_dob.Text = dob;
+
+                            // load profilePhoto
+                            // available picture types: square (50x50), small (50xvariable height), large (about 200x variable height) (all size in pixels)
+                            // for more info visit http://developers.facebook.com/docs/reference/api
+                            string profilePictureUrl = string.Format("https://graph.facebook.com/{0}/picture?type={1}", id, "square");
+                            BitmapImage image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(profilePictureUrl);
+                            image.EndInit();
+                            profilePhoto.Source = image;
+                        }); 
+                    }
+                };
+
+                // additional parameters can be passed and 
+                // must be assignable from IDictionary<string, object> or anonymous object
+                var parameters = new Dictionary<string, object>();
+                parameters["fields"] = "id,name,first_name,last_name,gender,birthday";
+
+                //fb.GetAsync("me", parameters);
+                fb.GetTaskAsync("me", parameters);
+            }
+            catch (FacebookApiException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
